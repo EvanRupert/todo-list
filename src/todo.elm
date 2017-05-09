155e58c2774dfc68da46029ef5a1db7e9c42f5eb
Eguy45 <rupert.e99@gmail.com>
2017-05-09 18:28:17 -0500

    Initial Commit

---
+++ src/todo.elm
@@ -0,0 +1,130 @@
+port module Todo exposing (..)
+
+
+import Html exposing (..)
+import Html.Events exposing ( onInput, onClick )
+import Html.Attributes exposing (..)
+
+
+--TODO: figure out how to get the styling correct
+--TODO: add 'event' for pressing the enter key to submit item
+--FIXME: fix length issue for task strings
+
+
+type alias Item =
+    { content : String
+    , id : Int
+    , completed : Bool
+    }
+
+
+
+type alias Model =
+    { content : List Item
+    , cItem : Item
+    , cId : Int
+    }
+
+
+type Msg 
+    = Editing String
+    | Submit
+--  | Remove Int
+
+
+main =
+    beginnerProgram { model = blankModel, view = view, update = update }
+
+
+blankModel : Model
+blankModel = 
+    --TODO: return start to empty list after testing
+    { content = testList
+    , cItem = blankItem
+    , cId = 0
+    }
+
+
+testList : List Item  
+testList =
+    [ { content = "Something", id = 0, completed = False }
+    , { content = "Something Else", id = 1, completed = False }
+    ]
+
+
+blankItem : Item
+blankItem =
+    { content = ""
+    , id = 0
+    , completed = False
+    }
+
+
+view : Model -> Html Msg
+view model =
+    div [ id "main" ]
+        [ h1 [ id "title" ] [ text "Todo List" ]
+        , div [ id "list-div" ] [ol [ id "list" ] <| List.map itemToHtml model.content]
+        , input [ placeholder "What to do...?"
+                , onInput Editing
+                , value model.cItem.content
+                ] []
+        , button [ onClick Submit ] [ text "Add" ]
+        ]
+
+
+itemToHtml : Item -> Html Msg
+itemToHtml item =
+    let 
+        listify node = 
+            if item.completed then
+                li [ class "completed" ] [ node ]
+            else
+                li [] [ node ]
+    in
+        div [ class "item-div" ]
+            [ button [ class "task-thing" ] [ text "O" ]
+            , p [ class "task-thing", class "task-content" ] [ text item.content ]
+            , button [ class "task-thing" ] [ text "X" ]
+            ]
+        |> listify
+
+
+update : Msg -> Model -> Model
+update msg model =
+
+    let
+        current = model.cItem
+    in
+        case msg of
+            Editing s ->
+                { model | cItem = { current | content = s } }
+            
+            Submit ->
+                { model 
+                | content = model.content ++ [current]
+                , cItem = blankItem
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

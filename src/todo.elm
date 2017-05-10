bfc9442e5c0d3a02e8147b10f6378732d5bde882
Eguy45 <rupert.e99@gmail.com>
2017-05-10 16:58:57 -0500

    Finished elm code

--- src/todo.elm
+++ src/todo.elm
@@ -2,19 +2,25 @@ port module Todo exposing (..)
 
 
 import Html exposing (..)
-import Html.Events exposing ( onInput, onClick )
+import Html.Events exposing ( onInput, onClick, on, keyCode )
 import Html.Attributes exposing (..)
 import Char exposing (fromCode)
+import Json.Decode as Json
 
 
---TODO: add events for marking task as completed and for deleting task
---TODO: styalize the input text field and 'add' button
+--FIXME: list items do not update until the filter has been changed
+    --IDEA: this could be done by moving the actual filtering to the
+    -- view function as a part of the listItems assignment
+    -- this would also allow the 'visible' field to be discarded
+    -- and the ChangeFilter match to only change the 'filter' field in
+    -- the model
 
 
 type alias Item =
     { content : String
     , id : Int
     , completed : Bool
+    , visible : Bool
     }
 
 
@@ -23,13 +29,23 @@ type alias Model =
     { content : List Item
     , cItem : Item
     , cId : Int
+    , filter : Filter
     }
+
+
+type Filter
+    = AllFilter
+    | CompletedFilter
+    | UnfinishedFilter
 
 
 type Msg 
     = Editing String
     | Submit
---  | Remove Int
+    | Remove Int
+    | Completed Int
+    | ClearCompleted
+    | ChangeFilter Filter
 
 
 main =
@@ -38,18 +54,12 @@ main =
 
 blankModel : Model
 blankModel = 
-    --TODO: return start to empty list after testing
-    { content = testList
+    { content = []
     , cItem = blankItem
     , cId = 0
+    , filter = AllFilter
     }
 
-
-testList : List Item  
-testList =
-    [ { content = "Something", id = 0, completed = False }
-    , { content = "Something Else", id = 1, completed = False }
-    ]
 
 
 blankItem : Item
@@ -57,42 +67,142 @@ blankItem =
     { content = ""
     , id = 0
     , completed = False
+    , visible = True
     }
+
+
+--VIEW
 
 
 view : Model -> Html Msg
 view model =
-    div [ id "main" ]
-        [ h1 [ id "title" ] [ text "Todo List" ]
-        , div [ id "list-div" ] [ol [ id "list" ] <| List.map itemToHtml model.content]
-        , input [ placeholder "What to do...?"
-                , onInput Editing
-                , value model.cItem.content
-                ] []
-        , button [ onClick Submit ] [ text "Add" ]
-        ]
+    let
+        items = model.content
+
+        itemFilter =
+            case model.filter of
+                AllFilter -> identity
+                CompletedFilter -> List.filter .completed
+                UnfinishedFilter -> List.filter (not << .completed)
+
+    in
+        div [ id "main" ]
+            [ h1 [ id "title" ] [ text "Todo List" ]
+            , input [ placeholder "What to do...?"
+                    , onInput Editing
+                    , onEnter Submit
+                    , value model.cItem.content
+                    , id "input-field"
+                    ] []
+            , button [ onClick Submit
+                    , disableIfEmpty model
+                    ] [ text "Submit" ]
+            , filtersBar model
+            , div [ id "list-div" ] 
+                [ ol [ id "list" ] 
+                    (items
+                    |> itemFilter
+                    |> List.map itemToHtml)
+                ]
+            ]
+
+
+disableIfEmpty : Model -> Attribute Msg
+disableIfEmpty model =
+    if String.isEmpty model.cItem.content then
+        id "disabled-submit-button"
+    else
+        id "submit-button"
+
+
+clearCompletedButton : Model -> Html Msg
+clearCompletedButton model =
+    let
+        visibility =
+            if exists .completed model.content then
+                style [("visibility", "visible")]
+            else
+                style [("visibility", "hidden")]
+    in
+        button [ class "filter-button"
+               , visibility
+               , onClick ClearCompleted
+               ] [ text "Clear Finished Items" ]
+
+
+filtersBar : Model -> Html Msg
+filtersBar model = 
+    let
+        visibility = 
+            if List.isEmpty model.content then
+                style [("visibility", "hidden")]
+            else
+                style [("visibility", "visible")]
+    in
+        div [ id "filter-bar", visibility ]
+            [ button [ class "filter-button"
+                    , if model.filter == AllFilter then class "selected-filter-button" else class "nothing"
+                    , onClick <| ChangeFilter AllFilter
+                    ] [ text "All" ]
+            , button [ class "filter-button" 
+                     , if model.filter == UnfinishedFilter then class "selected-filter-button" else class "nothing"
+                     , onClick <| ChangeFilter UnfinishedFilter
+                     ] [ text "Unfinished" ]
+            , button [ class "filter-button"
+                    , if model.filter == CompletedFilter then class "selected-filter-button" else class "nothing"
+                    , onClick <| ChangeFilter CompletedFilter
+                    ] [ text "Finished" ]
+            , clearCompletedButton model
+            ]
 
 
 itemToHtml : Item -> Html Msg
 itemToHtml item =
     let 
-        listify node = 
+        ifCompleted item attrib =
             if item.completed then
-                li [ class "completed" ] [ node ]
-            else
-                li [] [ node ]
+                attrib
+            else 
+                class "nothing"
+
     in
-        div [ class "item-div" ]
-            [ div [ class "done-button-div" ] [ button [ class "done-button" ] [] ]
-            , div [ class "task-content-div" ] [ span [ class "task-content" ] [ text item.content ] ]
-            , div [ class "close-button-div" ] [ button [ class "close-button" ] [] ]
+        div [ class "item-div", ifCompleted item (class "completed") ]
+            [ div [ class "done-button-div" ] 
+                [ button [ class "done-button"
+                         , onClick <| Completed item.id                             
+                         ] [] 
+                ]
+            , div [ class "task-content-div" ] [ span [ class "task-content"
+                                                      , ifCompleted item (style [("text-decoration", "line-through")])
+                                                      ] [ text item.content ] 
+                                               ]
+            , div [ class "close-button-div" ] 
+                [ button 
+                    [ class "close-button"
+                    , onClick <|Remove item.id 
+                    ] [] 
+                ]
             ]
-        |> listify
+        |> \x -> li [] [x]
+
+
+onEnter : Msg -> Attribute Msg
+onEnter msg =
+    let 
+        isEnter code =
+            if code ==13 then
+                Json.succeed msg
+            else
+                Json.fail "not ENTER"
+    in
+        on "keydown" (Json.andThen isEnter keyCode)
+
+
+--UPDATE
 
 
 update : Msg -> Model -> Model
 update msg model =
-
     let
         current = model.cItem
     in
@@ -101,12 +211,73 @@ update msg model =
                 { model | cItem = { current | content = s } }
             
             Submit ->
-                { model 
-                | content = model.content ++ [current]
-                , cItem = blankItem
+                if not <| String.isEmpty current.content then
+                    { model
+                    | content = current :: model.content
+                    , cItem = { blankItem | id = model.cId + 1 }
+                    , cId = model.cId + 1
+                    }
+                else
+                    model
+            
+            Remove idx ->
+                { model
+                | content = List.filter (\i -> i.id /= idx) model.content
                 }
 
+            Completed idx ->
+                { model
+                | content = List.map (\i ->
+                    if i.id == idx && i.completed then
+                        { i | completed = False }
+                    else if i.id == idx then
+                        { i | completed = True }
+                    else
+                        i
+                    ) model.content
+                }
 
+            ClearCompleted ->
+                { model 
+                | content = List.filter ( not << .completed ) model.content
+                }
+
+            ChangeFilter fil ->
+                { model | filter = fil }
+                    -- AllFilter -> 
+                    --     { model
+                    --     | content = List.map (\i ->
+                    --         { i | visible = True } ) model.content
+                    --     , filter = AllFilter
+                    --     }
+
+                    -- CompletedFilter -> 
+                    --     { model 
+                    --     | content = List.map (\i ->
+                    --         if i.completed then
+                    --             { i | visible = True }
+                    --         else
+                    --             { i | visible = False }) model.content
+                    --     , filter = CompletedFilter
+                    --     }
+
+                    -- UnfinishedFilter ->
+                    --     { model
+                    --     | content = List.map (\i ->
+                    --         if i.completed then 
+                    --             { i | visible = False }
+                    --         else
+                    --             { i | visible = True }) model.content
+                    --     , filter = UnfinishedFilter
+                    --     }
+
+
+--UTILITY
+
+
+exists : (a -> Bool) -> List a -> Bool
+exists f =
+    List.foldl (\x acc -> if f x then True else acc) False
 
 
 
